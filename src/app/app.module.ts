0a09097113ddc3fce163e8c517767455820ec377
ib4717 <ib4717@srmist.edu.in>
2022-07-14 00:08:51 +0530

    added add contact

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddContactComponent } from './add-contact/add-contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddContactComponent
   ],
   imports: [
     BrowserModule,
