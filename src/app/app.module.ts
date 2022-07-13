3bd838045938c40c547cd712086f758e4ef5b09d
Abhinav-3009 <abhi30092000.ag@gmail.com>
2022-07-14 00:02:40 +0530

    added edit contact

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EditContactComponent } from './edit-contact/edit-contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EditContactComponent
   ],
   imports: [
     BrowserModule,
